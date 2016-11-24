951a0308084f33b576ed8461d4577a0b660fd8e3
Pablo <pmmr@correo.ugr.es>
2016-11-25 00:04:17 +0100

    Arreglando Chef

--- Provisionamiento/Chef/chef/solo.rb
+++ Provisionamiento/Chef/chef/solo.rb
@@ -1,3 +1,3 @@
-file_cache_path "~/CineForYou/Provisionamiento/Chef/chef"
-cookbook_path "~/CineForYou/Provisionamiento/Chef/chef/cookbooks"
-json_attribs "~/CineForYou/Provisionamiento/Chef/chef/node.json"
+file_cache_path "/home/ubuntu/CineForYou/Provisionamiento/Chef/chef"
+cookbook_path "/home/ubuntu/CineForYou/Provisionamiento/Chef/chef/cookbooks"
+json_attribs "/home/ubuntu/CineForYou/Provisionamiento/Chef/chef/node.json"
