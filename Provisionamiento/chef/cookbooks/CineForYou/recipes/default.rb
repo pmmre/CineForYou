b3d2e9110b4949c6fa7c2c7bb18f04bc3177c6a4
Pablo <pmmr@correo.ugr.es>
2016-11-24 22:37:36 +0100

    Subiendo aprovisonamiento chef para probar en AWS

---
+++ Provisionamiento/chef/cookbooks/CineForYou/recipes/default.rb
@@ -0,0 +1,20 @@
+apt_update 'all platforms'
+apt_upgrade 'all platforms'
+
+package 'python-setuptools'
+package 'build-essential'
+package 'python-dev'
+package 'libpq-dev'
+package 'mongodb-server'
+package 'python-pip'
+
+easy_install_package 'Django'
+
+
+
+
+
+
+
+
+
---
+++ Provisionamiento/chef/solo.rb
@@ -0,0 +1,3 @@
+file_cache_path "/home/pablo/chef"
+cookbook_path "/home/pablo/chef/cookbooks"
+json_attribs "/home/pablo/chef/node.json"
